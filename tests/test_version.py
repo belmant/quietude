2d3fbea1c46b8ad6ff09570496b90218b91a4120
belmant <belmant@etud.insa-toulouse.fr>
2020-04-12 00:42:28 +0200

    Removed pythonpackage.yml

--- tests/test_version.py
+++ tests/test_version.py
@@ -8,9 +8,3 @@ def test_versionID_pass():
     expect_version = (0, 0, 1)
     return_version = get_version(ADDON)
     assert expect_version == return_version
-
-
-def test_versionID_fail():
-    expect_version = (0, 1, 1)
-    return_version = get_version(ADDON)
-    assert not expect_version == return_version
